bd8eb72424d784614f78dce7d1b26237af55f61a
Pegbeer <carlosgarciahl@hotmail.com>
2024-09-11 16:48:31 -0600

    Fix on artifact file path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ publishing{
             groupId = "me.pegbeer.commons"
             artifactId = "commons"
             version = "1.0.0"
-            artifact("${layout.buildDirectory}/outputs/aar/app-release.aar")
+            artifact("/app/outputs/aar/app-release.aar")
         }
     }
     repositories{
